32ce5e0cc16ffce69473b8bfc4a910c22ae3ea96
NatCanCode <nathalie.piat1@gmail.com>
2022-11-10 12:04:17 +0100

    Initial Commit

---
+++ RecipeTest/RecipeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeTestApp.swift
+//  RecipeTest
+//
+//  Created by N N on 10/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct RecipeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
